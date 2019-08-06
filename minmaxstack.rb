feac9e3658015e79b44da91b9a6c9a09b47d2de3
Anthony Garino <Anthonygarino@utexas.edu>
2019-08-06 00:31:38 -0500

    MinMaxStack

---
+++ minmaxstack.rb
@@ -0,0 +1,31 @@
+require "byebug"
+require_relative 'MyStack'
+
+class MinMaxStack
+
+  attr_reader :store
+
+  def initialize
+    @store = MyStack.new
+  end
+
+  def empty?
+    store.empty?
+  end
+
+  def min
+    peek[:min] unless empty?
+  end
+
+  def max
+    # byebug
+    peek[:max] unless empty?
+  end
+
+  def peek
+    store.peek unless empty?
+  end
+
+  def pop
+    store.pop[:value] unless empty?
+  end
