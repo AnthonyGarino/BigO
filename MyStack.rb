4cd12dafd712459c7ebb10c43ab594d793df1361
Anthony Garino <Anthonygarino@utexas.edu>
2019-08-06 00:00:07 -0500

    adMyStack

---
+++ MyStack.rb
@@ -0,0 +1,25 @@
+class MyStack
+    def initialize
+        @store = []
+    end
+
+    def peek
+        @store.first
+    end
+
+    def size 
+        @store.size
+    end
+
+    def empty?
+        @store.empty?
+    end
+
+    def pop
+        @store.pop
+    end
+
+    def push(rabbit)
+        @store << rabbit
+    end
+end
